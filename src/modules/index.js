87e4bea803bacd7bb0f2072f9ff5769585602d96
Richard Pietsch <31843706+RichardPietsch@users.noreply.github.com>
2026-02-25 16:38:10 +0100

    Add localization foundation with centralized UI dictionaries

---
+++ src/modules/index.js
@@ -0,0 +1,36 @@
+const { registerPeopleRoutes } = require('./people/routes');
+const { registerClientsRoutes } = require('./clients/routes');
+const { registerOnboardingRoutes } = require('./onboarding/routes');
+
+function registerModuleRoutes(app, deps) {
+  registerPeopleRoutes(app, {
+    pool: deps.pool,
+    badRequest: deps.badRequest,
+    handleDbError: deps.handleDbError,
+    parseOptionalBoolean: deps.parseOptionalBoolean,
+    parseWorkingHours: deps.parseWorkingHours,
+    requirePermission: deps.requirePermission,
+    PERMISSIONS: deps.PERMISSIONS
+  });
+
+  registerClientsRoutes(app, {
+    pool: deps.pool,
+    badRequest: deps.badRequest,
+    handleDbError: deps.handleDbError,
+    requireMonth: deps.requireMonth,
+    requirePermission: deps.requirePermission,
+    PERMISSIONS: deps.PERMISSIONS
+  });
+
+  registerOnboardingRoutes(app, {
+    pool: deps.pool,
+    badRequest: deps.badRequest,
+    handleDbError: deps.handleDbError,
+    requirePermission: deps.requirePermission,
+    PERMISSIONS: deps.PERMISSIONS
+  });
+}
+
+module.exports = {
+  registerModuleRoutes
+};
